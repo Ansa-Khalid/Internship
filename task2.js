60d1e8fd170cce5415ec91daed36d48ae283bf52
Ansa <ansakhalid3@gmail.com>
2023-07-18 16:22:01 +0500

    Second commit

---
+++ task2.js
@@ -0,0 +1,57 @@
+// Write an arithmetic expression that calculates the area of a rectangle with a width of 5 and a height
+// of 8.
+var height=8, width=5;
+var area=width*height;
+console.log(area);
+
+// Create a comparison expression that checks if two numbers are equal.
+var x=5, y=4;
+console.log(x==y);
+
+// Write a logical expression that checks if a number is both greater than 5 and less than 10.
+var num=6;
+console.log(num>5&&num<10);
+
+// Concatenate two strings using the concatenation operator.
+var fname="Ansa"
+var lname="Khalid"
+console.log(fname+" "+ lname);
+
+// Increment a variable counter by 1 using the increment operator.
+var counter=1;
+console.log('Prefix Increment:'+ ++counter);
+console.log('Posfix Increment:'+ counter++);
+
+
+// Decrement a variable counter by 1 using the decrement operator.
+var counter=1;
+console.log('Prefix Decrement:'+ --counter);
+console.log('Posfix Decrement:'+ counter--);
+
+
+
+// Calculate the remainder when dividing 17 by 3 using the remainder operator (%).
+var rem = 17%3;
+console.log(rem);
+
+
+
+// Assign the value of 10 to a variable x and use the addition assignment operator to add 5 to x.
+var x=10;
+x+=5;
+console.log(x);
+
+
+// Write a comparison expression using the strict equality operator that checks if a number and a
+// string are equal.
+var n1=4,n2="4";
+console.log(n1===n2);
+
+// Use the ternary operator to check if a variable isRaining is true. If true, assign the value "Take
+// an umbrella" to a variable advice; otherwise, assign "Enjoy the day" to advice." to advice.
+var isRaining = true;
+var rain='Take an umbrella';
+var noRain='Enjoy the day'
+console.log((isRaining)?rain:noRain);
+    
+
